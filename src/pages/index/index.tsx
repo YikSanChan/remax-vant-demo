0bccf994af3203b0af9bb03f3a4db57472854523
Evan Chan <evan.chanyiksan@gmail.com>
2020-05-22 21:17:53 -0700

    content-class doesn't work

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -8,13 +8,13 @@ export default () => {
   return (
     <View className={styles.app}>
       <VantGrid column-num={7} border={false}>
-      <VantGridItem key="1" text="Mon" custom-class="color:red" />
-      <VantGridItem key="2" text="Tue" custom-class="background-color:red"/>
-      <VantGridItem key="3" text="Wed" text-class="color:red" />
-      <VantGridItem key="4" text="Thu" />
-      <VantGridItem key="5" text="Fri" />
-      <VantGridItem key="6" text="Sat" />
-      <VantGridItem key="7" text="Sun" />
+        <VantGridItem key="1" text="Mon" icon="photo-o" dot content-class="grid-item-color" />
+        <VantGridItem key="2" text="Tue" icon="photo-o" />
+        <VantGridItem key="3" text="Wed" icon="photo-o" />
+        <VantGridItem key="4" text="Thu" icon="photo-o" />
+        <VantGridItem key="5" text="Fri" icon="photo-o" />
+        <VantGridItem key="6" text="Sat" icon="photo-o" />
+        <VantGridItem key="7" text="Sun" icon="photo-o" />
       </VantGrid>
     </View>
   );
